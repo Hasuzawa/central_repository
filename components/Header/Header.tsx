7e407c1ba08a4726693e6b76a1b37085604318ff
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2021-09-13 01:01:15 +0900

    added <DayNightSwitch>, cleaned up <Header>

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,31 +1,37 @@
-import { useContext } from "react";
+import { useContext, Dispatch, SetStateAction} from "react";
 import DarkModeContext from "../contexts/DarkModeContext";
+import { Sun, Moon } from "phosphor-react";
 
-
-import styles from "~/styles/moduleCSS/button.module.css";
 
 const Header = (): JSX.Element => {
     const darkModeContext = useContext(DarkModeContext);
     const inDarkMode = darkModeContext.inDarkMode;
     const setDarkMode = darkModeContext.setDarkMode;
-
-    const btnClassName: string = inDarkMode ? styles.moonBtn : styles.sunBtn;
-    //(inDarkMode ? styles.moon-btn : "sun-btn")
+    if (setDarkMode === null){throw new Error();};
 
     return (
         <div id="header" className="h-16 w-screen flex-none sticky top-0 flex flex-row border-b-2 border-black shadow_around z-50 dark:bg-black dark:text-white dark:border-white">
             <span>logo</span>
-            <span>day/night button</span>
-            <button
-                onClick={ () => setDarkMode(!inDarkMode)}
-                className={[styles.darkModeBtn, btnClassName].join(" ")}
-            >
-                this is day night button
-            </button>
-            <p className="dark:text-green-500">current dark mode value is {inDarkMode.toString()}</p>
+            <DayNightSwitch inDarkMode={inDarkMode} setDarkMode={setDarkMode} />
+            
+            {/* <p className="dark:text-green-500">current dark mode value is {inDarkMode.toString()}</p> */}
         </div>
     );
 }
 
 
+
+interface DayNightSwitchProps{
+    inDarkMode: boolean;
+    setDarkMode: Dispatch<SetStateAction<boolean>>;
+}
+
+const DayNightSwitch = ({inDarkMode, setDarkMode}: DayNightSwitchProps): JSX.Element => {
+
+    return inDarkMode ? <Moon size={48} onClick={() => setDarkMode(!inDarkMode)} />
+                      :< Sun size={48} onClick={() => setDarkMode(!inDarkMode)} />
+};
+
+
+export { DayNightSwitch };
 export default Header;
--- components/contexts/DarkModeContext.tsx
+++ components/contexts/DarkModeContext.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {Dispatch, SetStateAction} from "react";
 /**
  * @remarks
  * use this context at the top of each page to access dark state and its setter
  */
-const DarkModeContext = React.createContext<{inDarkMode: boolean, setDarkMode: any}>({inDarkMode: false, setDarkMode: null});
+const DarkModeContext = React.createContext<{inDarkMode: boolean, setDarkMode: null | Dispatch<SetStateAction<boolean>>}>
+({inDarkMode: false, setDarkMode: null});
 export default DarkModeContext;
